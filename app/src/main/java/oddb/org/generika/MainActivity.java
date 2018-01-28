d7c4726175af485f572277e94ae3d476b9f2053e
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-01-29 02:03:14 +0900

    Add drawer navigation view

--- app/src/main/java/oddb/org/generika/MainActivity.java
+++ app/src/main/java/oddb/org/generika/MainActivity.java
@@ -2,53 +2,85 @@ package oddb.org.generika;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  private DrawerLayout mDrawerLayout;
 
-        FloatingActionButton fab = (FloatingActionButton)findViewById(
-            R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(
-                  view, "Replace with your own action", Snackbar.LENGTH_LONG
-                ).setAction("Action", null).show();
-            }
-        });
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it exists.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    ActionBar actionBar = getSupportActionBar();
+    actionBar.setDisplayHomeAsUpEnabled(true);
+
+    mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+    NavigationView navigationView = (NavigationView)findViewById(
+      R.id.navigation_view);
+    navigationView.setNavigationItemSelectedListener(
+      new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem menuItem) {
+          menuItem.setChecked(true);
+          mDrawerLayout.closeDrawers();
+          Toast.makeText(
+            MainActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+          return true;
+        }
+
+    });
+
+    FloatingActionButton fab = (FloatingActionButton)findViewById(
+      R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(
+          view, "Replace with your own action", Snackbar.LENGTH_LONG
+        ).setAction("Action", null).show();
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it exists.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    switch (id) {
+      case android.R.id.home:
+        mDrawerLayout.openDrawer(GravityCompat.START);
+        return true;
+      case R.id.action_settings:
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
