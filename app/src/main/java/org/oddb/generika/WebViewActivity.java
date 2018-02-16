7e0dc81423b7125ec3832836e5d24d90372ecfbb
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-02-16 23:22:09 +0900

    Create WebView
    * Enable pinch zoom
    * Enable JavaScript
    * Fix viewport (unexpected zoom-in)
    * Add inner web client (open all links in this browser)
    * Add history management in web client
    * Add device back button support
    * Fix scrollbar length and state (fade out)
    * Just open ch.oddb.org (top page)

--- app/src/main/java/org/oddb/generika/WebViewActivity.java
+++ app/src/main/java/org/oddb/generika/WebViewActivity.java
@@ -18,33 +18,103 @@
 package org.oddb.generika;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
 
 public class WebViewActivity extends AppCompatActivity {
 
+  private WebView webView;
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    Context context = (Context)this;
     setContentView(R.layout.activity_web_view);
+
+    initViews();
+
+    // TODO: get urlString (intent)
+    String urlString = "https://i.ch.oddb.org/";
+
+    if (webView != null && savedInstanceState == null) {
+      webView.loadUrl(urlString);
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    this.webView = null;
+
+    super.onDestroy();
+  }
+
+  private void initViews() {
+    Context context = (Context)this;
 
     Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
     toolbar.setTitle(context.getString(R.string.oddb_org));
-    toolbar.setSubtitle("https://ch.oddb.org/...");
+    toolbar.setSubtitle("https://i.ch.oddb.org/...");
     setSupportActionBar(toolbar);
 
     ActionBar actionBar = getSupportActionBar();
     actionBar.setDisplayHomeAsUpEnabled(true);
     actionBar.setDisplayShowHomeEnabled(true);
 
-    TextView textView = (TextView)findViewById(R.id.text_view);
-    textView.setText(context.getString(R.string.web_view_text));
+    this.webView = (WebView)findViewById(R.id.web_view);
+    webView.setWebViewClient(new WebViewClient());
+    webView.setPadding(0, 0, 0, 0);
+    webView.setScrollbarFadingEnabled(true);
+    webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+    WebSettings webSettings = webView.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+
+    // fix initial zoom-in (as zoom-out at start)
+    webView.setInitialScale(1);
+    webSettings.setUseWideViewPort(true);
+
+    // allow (pinch) zoom
+    webSettings.setBuiltInZoomControls(true);
+    webSettings.setDisplayZoomControls(false);
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    // on tap `◁` (device back button)
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+      webView.goBack();
+      return true;
+    }
+    return super.onKeyDown(keyCode, event);
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    // do nothing
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+
+    webView.saveState(outState);
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+
+    webView.restoreState(savedInstanceState);
   }
 
   @Override
