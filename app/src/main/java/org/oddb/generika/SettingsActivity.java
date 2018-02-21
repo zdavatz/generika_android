d7b135630eecb8938e076275b70bddad9d5db2eb
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-02-21 21:20:37 +0900

    Add settings view
    * Create settings activity and fragment
    * Create custom ListPreference (to set current value as summary)
    * Add search type (FI, PI und preis vergleich)
    * Add search language (de und fr)

---
+++ app/src/main/java/org/oddb/generika/SettingsActivity.java
@@ -0,0 +1,67 @@
+/*
+ *  Generika Android
+ *  Copyright (C) 2018 ywesee GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.oddb.generika;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+  private final static String kSearchType = "kSearchType";
+  private final static String kSearchLang = "kSearchLang";
+  // TODO: enable cloud storage support
+  //private final static String kRecordSync = "kRecordSync";
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
+
+    initViews();
+  }
+
+  private void initViews() {
+    Context context = (Context)this;
+
+    Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+    toolbar.setTitle(context.getString(R.string.settings));
+    setSupportActionBar(toolbar);
+
+    ActionBar actionBar = getSupportActionBar();
+    actionBar.setDisplayHomeAsUpEnabled(true);
+    actionBar.setDisplayShowHomeEnabled(true);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == android.R.id.home) {
+      finish();
+    }
+    return super.onOptionsItemSelected(item);
+  }
+}
---
+++ app/src/main/java/org/oddb/generika/preference/AppListPreference.java
@@ -0,0 +1,63 @@
+/*
+ *  Generika Android
+ *  Copyright (C) 2018 ywesee GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.oddb.generika.preference;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.ListPreference;
+import android.util.AttributeSet;
+
+import java.lang.CharSequence;
+import java.lang.Object;
+
+
+public class AppListPreference extends ListPreference {
+
+  private final static String TAG = AppListPreference.class.getName();
+
+  public AppListPreference(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    initOnChangeListener();
+  }
+
+  public AppListPreference(Context context) {
+    super(context);
+
+    initOnChangeListener();
+  }
+
+  private void initOnChangeListener() {
+    // set current value as summary of preference
+    setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+      @Override
+      public boolean onPreferenceChange(
+        Preference preference, Object _newValue) {
+
+        preference.setSummary(getEntry());
+        return true;
+      }
+    });
+  }
+
+  @Override
+  public CharSequence getSummary() {
+    return super.getEntry();
+  }
+}
