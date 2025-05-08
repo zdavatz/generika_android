eafd34efdcf874c90acc8915518a7c236953350e
b123400 <i@b123400.net>
2025-05-08 23:01:01 +0900

    Apply saved profile #98

--- app/src/main/java/org/oddb/generika/barcode/EPrescription.java
+++ app/src/main/java/org/oddb/generika/barcode/EPrescription.java
@@ -1,15 +1,19 @@
 package org.oddb.generika.barcode;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.util.Base64;
 import android.util.Log;
+
+import androidx.preference.PreferenceManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.oddb.generika.model.Receipt;
 import org.oddb.generika.model.ZurRosePrescription;
+import org.oddb.generika.util.Constant;
 import org.oddb.generika.util.Hash;
 import org.oddb.generika.util.StreamReader;
 
@@ -29,6 +33,7 @@ import java.util.zip.GZIPInputStream;
 
 public class EPrescription {
     private final static String TAG = "EPrescription";
+    private SharedPreferences sharedPreferences;
 
     class PatientId {
         int type;
@@ -89,7 +94,8 @@ public class EPrescription {
     ArrayList<PField> patientPFields;
 
     ArrayList<Medicament> medicaments;
-    public EPrescription(String qrCodeString) throws IllegalArgumentException, IOException, JSONException {
+    public EPrescription(Context context, String qrCodeString) throws IllegalArgumentException, IOException, JSONException {
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         if (qrCodeString.startsWith("https://eprescription.hin.ch")) {
             int sharpIndex = qrCodeString.indexOf("#");
             qrCodeString = qrCodeString.substring(sharpIndex + 1);
@@ -114,7 +120,8 @@ public class EPrescription {
         this.date = this.parseDateString(obj.optString("Dt"));
         this.prescriptionId = obj.optString("Id", "");
         this.medType = obj.optInt("MedType", 3); // Default 3 = Prescription
-        this.zsr = obj.optString("Zsr", "");
+        String savedZsr = this.sharedPreferences.getString(Constant.kProfileZSR, "");
+        this.zsr = !savedZsr.isEmpty() ? savedZsr : obj.optString("Zsr", "");
         this.rmk = obj.optString("rmk", "");
 
         ArrayList<PField> pfs = new ArrayList<>();
@@ -323,10 +330,12 @@ public class EPrescription {
         prescriptor.lastName = this.auth; // ???
 
         prescriptor.langCode = 1;
-        prescriptor.clientNrClustertec = "888870";
+        String savedZrCustomer = this.sharedPreferences.getString(Constant.kProfileZrCustomerNumber, "");
+        prescriptor.clientNrClustertec = !savedZrCustomer.isEmpty() ? savedZrCustomer : "888870";
         prescriptor.street = "";
         prescriptor.zipCode = "";
         prescriptor.city = "";
+        prescriptor.eanId = this.sharedPreferences.getString(Constant.kProfileGLN, "");
 
         String insuranceEan = null;
         String coverCardId = null;
